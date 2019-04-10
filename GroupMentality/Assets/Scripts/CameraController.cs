c86d32f633fda873337be66709ec792d771550c6
Charles <lycoinjunior@gmail.com>
2019-04-10 23:14:31 +0200

    Skyboxes, Student Movement Scripts
    * Added a pretty skyboxes
    * Added code for student movement. So far, normal students can follow the teacher mostly wherever they go, within a threshold distance.
    * Changed the colliders on the streets from Mesh Colliders to simple box colliders.

--- GroupMentality/Assets/Scripts/CameraController.cs
+++ GroupMentality/Assets/Scripts/CameraController.cs
@@ -1,32 +1,56 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 // replace this very simple camera logic with a better solution
 public class CameraController : MonoBehaviour {
-
     public Transform target;
+    //public float distance;
+    //float angleX;
+   //float angleY;
+
+    //void Update () {
+    //       Vector3 v = new Vector3(0, 0, 1);
+    //       float x = Input.GetAxis("Mouse X");
+    //       angleX += x * 10;
+    //       float y = Input.GetAxis("Mouse Y");
+    //       angleY += y * 10;
+
+    //       Quaternion rx = Quaternion.AngleAxis(angleX, new Vector3(0, 1, 0));
+    //       Vector3 v_prime = rx * v;
+
+    //       Quaternion ry = Quaternion.AngleAxis(angleY, new Vector3(1, 0, 0));
+    //       Vector3 v_doublePrime = ry * v_prime; 
+
+    //	transform.position = v_doublePrime * distance + target.position;
+    //	transform.LookAt (target);
+    //}
+
+
 
     float y_axis;
     float x_axis;
+    public float distance = 5;
 
-    void Start () {
+    void Start()
+    {
         y_axis = Input.GetAxis("Mouse Y");
         x_axis = Input.GetAxis("Mouse X");
     }
-		
-	void Update () {
+
+    void Update()
+    {
 
         transform.position = new Vector3(0, 3, -3);
         transform.LookAt(target);
 
 
-        y_axis += Input.GetAxis("Mouse Y");
+        y_axis += Input.GetAxis("Mouse Y") * 10;
         float y_axis_clamped = Mathf.Clamp(y_axis, -30, 0F);
 
-        x_axis += Input.GetAxis("Mouse X");
+        x_axis += Input.GetAxis("Mouse X") * 10;
         float x_axis_clamped = Mathf.Clamp(x_axis, -360F, 360F);
-       
-        Vector3 distance = transform.position - target.position;
+
+        //distance = (transform.position - target.position).magnitude;
 
         Vector3 v = new Vector3(0, 0, 1);
 
@@ -39,14 +63,18 @@ public class CameraController : MonoBehaviour {
         Vector3 v_prime_prime = rotation2 * v_prime;
 
 
-        Vector3 vector_to_target = v_prime_prime * distance.magnitude;
+        Vector3 vector_to_target = v_prime_prime * distance;
 
         // find the player position
         Vector3 cameraPosition = target.position + vector_to_target;
 
 
         transform.position = cameraPosition;
-		transform.LookAt (target);
-	}
-		
+        transform.LookAt(target);
+    }
+
 }
+
+		
+
+
---
+++ GroupMentality/Assets/Scripts/NormalStudent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalStudent : Student
+{
+
+    public Transform target; // This is Benno's transform
+    private Rigidbody rb2d;
+
+    void Start()
+    {
+        speed = 5f;
+        randomness = Random.Range(1f, 3f);
+        minDistanceToFollow = 5f;
+
+        rb2d = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity = new Vector3(0, 0, 0);
+        Move();
+    }
+
+    public override void Move()
+    {
+        Vector3 origin = transform.position;
+
+        Vector3 direction = target.position - origin;
+
+        float distanceToPlayer = direction.magnitude;
+        Debug.Log(distanceToPlayer);
+
+        direction.Normalize();
+
+        if (distanceToPlayer > minDistanceToFollow)
+        {
+            rb2d.velocity = direction * speed * randomness;
+        }
+
+        else
+        {
+            rb2d.velocity = new Vector3(0, 0, 0);
+        }
+
+
+
+
+
+    }
+}
