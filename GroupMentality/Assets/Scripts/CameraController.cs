f9cee3566ef45ba64ecde9af945e60b9a83d396a
MatthewDerzay <mderzay@wisc.edu>
2019-04-11 15:19:42 +0200

    Got basic controls working

--- GroupMentality/Assets/Scripts/CameraController.cs
+++ GroupMentality/Assets/Scripts/CameraController.cs
@@ -1,34 +1,66 @@
-﻿using UnityEngine;
+﻿// <copyright file="CameraController.cs" company="DIS Copenhagen">
+// Copyright (c) 2017 All Rights Reserved
+// </copyright>
+// <author>Benno Lueders</author>
+// <date>05/10/2017</date>
+
+using UnityEngine;
 using System.Collections;
 
-// replace this very simple camera logic with a better solution
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     public Transform target;
-    public float distance = 5;
+
+    public LayerMask obstacleLayerMask;
+
+    public float distance = 10;
+    public float minVerticalAngle = -80;
+    public float maxVerticalAngle = 80;
+
+    public float verticalMouseSpeed;
+    public float horizontalMouseSpeed;
+    public float verticalArrowSpeed;
+    public float horizontalArrowSpeed;
 
     private float angleX;
     private float angleY;
 
-	void Start () {
+    void Start()
+    {
+        angleX = -45;
+        angleY = 0;
+    }
 
-	}
-		
-	void Update () {
-        Vector3 v = new Vector3(0, 0, 1);
-        float x = Input.GetAxis("Mouse X");
-        angleX += x * 10;
-        float y = Input.GetAxis("Mouse Y");
-        angleY += y * 10;
+    void Update()
+    {
+        angleX += ((Input.GetAxis("ArrowVertical") * verticalArrowSpeed) + (Input.GetAxis("Mouse Y") * verticalMouseSpeed)) * Time.deltaTime;
+        angleY += ((Input.GetAxis("ArrowHorizontal") * horizontalArrowSpeed) + (Input.GetAxis("Mouse X") * horizontalMouseSpeed)) * Time.deltaTime;
 
-        Quaternion rx = Quaternion.AngleAxis(angleX, new Vector3(0, 1, 0));
-        Vector3 v_prime = rx * v;
+        angleX = Mathf.Clamp(angleX, minVerticalAngle, maxVerticalAngle);
+        angleY %= 360;
 
-        Quaternion ry = Quaternion.AngleAxis(angleY, new Vector3(1, 0, 0));
-        Vector3 v_doublePrime = ry * v_prime; 
+        Quaternion xRotation = Quaternion.AngleAxis(angleX, new Vector3(1, 0, 0));
+        Quaternion yRotation = Quaternion.AngleAxis(angleY, new Vector3(0, 1, 0));
+        Vector3 offset = new Vector3(0, 0, 1);
+        offset = xRotation * offset;
+        offset = yRotation * offset;
+        offset *= distance;
 
-		transform.position = v_doublePrime * distance + target.position;
-		transform.LookAt (target);
-	}
-		
+        offset = AddObstacleAvoidance(offset);
+
+        transform.position = target.position + offset;
+        transform.rotation = Quaternion.LookRotation(target.position - transform.position, new Vector3(0, 1, 0));
+    }
+
+    Vector3 AddObstacleAvoidance(Vector3 targetToCamera)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(target.position, targetToCamera, out hit, distance, obstacleLayerMask))
+        {
+            // if we hit an object between camera and target position the camera at (in front of) the hit object.
+            return hit.point;
+        }
+        return targetToCamera;
+    }
 }
---
+++ GroupMentality/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Transform cam;
+    Vector2 input;
+
+    void FixedUpdate()
+    {
+        input = new Vector2(Input.GetAxis("ADHorizontal"), Input.GetAxis("WSVertical"));
+        input = Vector2.ClampMagnitude(input, 1);
+
+        Vector3 camF = cam.forward;
+        Vector3 camR = cam.right;
+
+        camF.y = 0;
+        camR.y = 0;
+        camF = camF.normalized;
+        camR = camR.normalized;
+
+        transform.position += (camF * input.y + camR * input.x) * Time.deltaTime * speed;
+    }
+}
