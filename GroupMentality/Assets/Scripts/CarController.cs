8a44f7fd3a88f6ea898a5d1d30d46cfb59270856
MatthewDerzay <mderzay@wisc.edu>
2019-04-12 12:52:30 +0200

    Added car model and movement

---
+++ GroupMentality/Assets/Scripts/CarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = Vector3.forward;
+
+        rb.AddForce(movement * speed);
+    }
+}
