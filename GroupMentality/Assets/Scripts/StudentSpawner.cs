cf20c39e50632b0faab49186ad6882a0da5864e2
Brandt <brandtdudziak@gmail.com>
2019-04-12 12:55:21 +0200

    Started student spawner

---
+++ GroupMentality/Assets/Scripts/StudentSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentSpawner : MonoBehaviour
+{
+    public Material[] materials;
+    public GameObject[] students;
+    public int numStudents;
+
+    void Start()
+    {
+        for(int i = 0; i < numStudents; i++)
+        {
+            int randomnumber = Random.Range(0, 4);
+            GameObject student = Instantiate(students[randomnumber], transform.position, transform.rotation);
+            randomnumber = Random.Range(0, 4);
+            Renderer rend = student.GetComponent<Renderer>();
+            rend.material = materials[randomnumber];
+        }
+    }
+
+}
