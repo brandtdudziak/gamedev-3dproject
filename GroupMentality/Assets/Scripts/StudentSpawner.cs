93e4f28952a6b9b8ad2b095f1963cd72f800b22c
Brandt <brandtdudziak@gmail.com>
2019-04-19 22:46:23 +0200

    Fixed student spawner
    Fixed student spawner so students don’t spawn within each other

--- GroupMentality/Assets/Scripts/StudentSpawner.cs
+++ GroupMentality/Assets/Scripts/StudentSpawner.cs
@@ -10,13 +10,32 @@ public class StudentSpawner : MonoBehaviour
 
     void Start()
     {
+        int zOffset = 2;
+        int xOffset = 0;
+        int offsetScale = 2;
+
         for(int i = 0; i < numStudents; i++)
         {
-            int randomnumber = Random.Range(0, 4);
-            GameObject student = Instantiate(students[randomnumber], transform.position, transform.rotation);
-            randomnumber = Random.Range(0, 4);
+
+            if(xOffset > 6)
+            {
+                xOffset = -2;
+                offsetScale *= -1;
+            } else if(xOffset < -6)
+            {
+                xOffset = -2;
+                offsetScale *= -1;
+                zOffset += offsetScale;
+            }
+
+            Vector3 pos = new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z + zOffset);
+            int randomnumber = Random.Range(0, students.Length);
+            GameObject student = Instantiate(students[randomnumber], pos, transform.rotation);
+            randomnumber = Random.Range(0, materials.Length);
             Renderer rend = student.GetComponent<Renderer>();
             rend.material = materials[randomnumber];
+
+            xOffset += offsetScale;
         }
     }
 
