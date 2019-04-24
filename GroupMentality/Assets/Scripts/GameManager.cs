929b8851f3ffab4ff21469fbb003da4c832641d7
Brandt <brandtdudziak@gmail.com>
2019-04-24 10:21:22 +0200

    Create Game Manager

---
+++ GroupMentality/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    private void Awake() {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
